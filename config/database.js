ae86bec80a3d3c35750b63826e5965d7a2702dbc
Naldio-pluralia <naldio.pluralia@gmail.com>
2019-05-14 16:35:05 +0100

    Add Migration on Heroku 3

--- config/database.js
+++ config/database.js
@@ -5,8 +5,6 @@ const Env = use("Env");
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use("Helpers");
-const Url = require("url-parse");
-const CLEARDB_DATABASE_URL = new Url(Env.get("CLEARDB_DATABASE_URL"));
 
 module.exports = {
   /*
@@ -75,7 +73,7 @@ module.exports = {
   pg: {
     client: "pg",
     connection: {
-      host: Env.get("DB_HOST", CLEARDB_DATABASE_URL.host),
+      host: Env.get("DB_HOST", "localhost"),
       port: Env.get("DB_PORT", ""),
       user: Env.get("DB_USER", "root"),
       password: Env.get("DB_PASSWORD", ""),
